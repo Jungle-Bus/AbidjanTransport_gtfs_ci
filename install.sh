e970c68ecd875b1739ac3e8dda1cfa09147319a7
Noémie Lehuby <noemie.lehuby@zaclys.net>
2021-05-31 20:18:43 +0200

    So long travis and thanks for all the fish

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ pip install -e .
 cd ..
 
 # install osm2gtfs
-git clone --branch=fix_shapes https://github.com/Jungle-Bus/osm2gtfs
+git clone --branch=new_ci https://github.com/Jungle-Bus/osm2gtfs
 
 cd osm2gtfs
 
