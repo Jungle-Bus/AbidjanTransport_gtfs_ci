78776ff6e6221b3ca007f55248b172b9865dc448
Noémie Lehuby <noemie.lehuby@zaclys.net>
2021-08-20 08:41:14 +0200

    Launch back GTFS generation

--- install.sh
+++ install.sh
@@ -6,18 +6,9 @@ set -ev
 wget https://github.com/mecatran/gtfsvtor/releases/download/v0.1.1/gtfsvtor.zip
 unzip gtfsvtor.zip
 
-#install transitfeed
-git clone --branch=python3 https://github.com/pecalleja/transitfeed.git
+# install prism
+git clone --branch=ci https://github.com/Jungle-Bus/prism
 
-cd transitfeed
+cd prism
 
-pip install -e .
-
-cd ..
-
-# install osm2gtfs
-git clone --branch=new_ci https://github.com/Jungle-Bus/osm2gtfs
-
-cd osm2gtfs
-
-pip install -e .
+poetry install
--- make_gtfs.sh
+++ make_gtfs.sh
@@ -2,18 +2,24 @@
 
 set -ev
 
+wget http://download.geofabrik.de/africa/ivory-coast-latest.osm.pbf --no-verbose --output-document=data.osm.pbf 2>&1
+
+osmium tags-filter data.osm.pbf type=route_master type=route -o pt_data.osm.pbf
+
 mkdir output
 
-osm2gtfs -c 2021_conf.json
+cd prism
 
-cp ./data/ci-abidjan.zip ./output/abidjan.zip
+poetry run python prism/cli.py ../pt_data.osm.pbf --outdir ../output/ --loglevel=WARNING --config ../2021_conf.json
 
-./gtfsvtor/bin/gtfsvtor output/abidjan.zip
+cd ..
+
+./gtfsvtor/bin/gtfsvtor output/output_gtfs.zip
 
 cp validation-results.html ./output/validation-results.html
 
 cd output
 
-unzip abidjan.zip
+unzip output_gtfs.zip
 
 cd ..
